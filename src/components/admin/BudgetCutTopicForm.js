9feaf5fd237e5ed9c2bc18af00dad05fab6b2a8a
Vinay <rmdort@gmail.com>
2015-11-25 14:29:18 +0800

    Added validation for topic and subtopics

--- src/components/admin/BudgetCutTopicForm.js
+++ src/components/admin/BudgetCutTopicForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import InputMaterial from '../InputMaterial';
+import {validationOptions} from './../../constants';
 
 var BudgetCutTopicForm = React.createClass({
 	getInitialState: function(){
@@ -18,49 +19,58 @@ var BudgetCutTopicForm = React.createClass({
 		}
 	},
 	onSave: function(event){
-
-		event && event.preventDefault();
-
-		var {budgetCutTopic, topic} = this.props;
-
-		if(budgetCutTopic){
-			// Edit form 
-			
-			this.props.flux.actions.AdminActions.editBudgetCutTopic({
-				name: this.state.budgetCutTopicName,
-				topicId: topic.id,
-				id: budgetCutTopic.id,
-				userId: CURRENT_USER.id
-			})			
-
-		}else{
-
-			this.props.flux.actions.AdminActions.createBudgetCutTopic({
-				name: this.state.budgetCutTopicName,
-				topicId: topic.id,
-				userId: CURRENT_USER.id
-			})
-		}
-
-		this.setState({
-			budgetCutTopicName: ''
-		});
 		
-		this.props.toggleAdd.call(this)
+		if(this.$form.valid()){
+
+			event && event.preventDefault();
+
+			var {budgetCutTopic, topic} = this.props;
+
+			if(budgetCutTopic){
+				// Edit form 
+				
+				this.props.flux.actions.AdminActions.editBudgetCutTopic({
+					name: this.state.budgetCutTopicName,
+					topicId: topic.id,
+					id: budgetCutTopic.id,
+					userId: CURRENT_USER.id
+				})			
+
+			}else{
+
+				this.props.flux.actions.AdminActions.createBudgetCutTopic({
+					name: this.state.budgetCutTopicName,
+					topicId: topic.id,
+					userId: CURRENT_USER.id
+				})
+			}
+
+			this.setState({
+				budgetCutTopicName: ''
+			});
+			
+			this.props.toggleAdd.call(this)
+
+		}
 
 	},
 	componentDidMount: function(){
 		this.refs.firstInput.getDOMNode().getElementsByTagName('input')[0].focus()
+
+		this.$form = $(this.refs.form.getDOMNode());
+
+		this.$form.validate(validationOptions);
 	},
 	render: function(){
 		
 		return (
-			<form className="section-form">
+			<form className="section-form" ref = "form" onSub>
 				<InputMaterial
 					label="Budget Cut Topic name"
 					name="topicName"
 					ref = "firstInput"
 					value = {this.state.budgetCutTopicName}
+					required = {true}
 					onChange = { (event)=> {
 						this.setState({
 							budgetCutTopicName: event.target.value
--- src/components/admin/NewTopic.js
+++ src/components/admin/NewTopic.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import InputMaterial from '../InputMaterial';
 import Fluxxor from 'fluxxor';
+import {validationOptions} from './../../constants';
 
 var FluxMixin = Fluxxor.FluxMixin(React);
 
-var NewTopic = React.createClass({
-	mixins: [FluxMixin],
+var NewTopic = React.createClass({	
 	getInitialState: function(){
 		var {topic} = this.props;
 
@@ -15,34 +15,42 @@ var NewTopic = React.createClass({
 		}
 	},
 	componentDidMount: function(){
-
+		
 		setTimeout(()=> {				
 			this.refs.firstInput.getDOMNode().getElementsByTagName('input')[0].focus()
 		}, 0)
 
+		this.$form = $(this.refs.form.getDOMNode());
+
+		this.$form.validate(validationOptions);
+
 	},
 	onSave: function(event){
 		
-		event && event.preventDefault();
+		if(this.$form.valid()){
+			
+			event && event.preventDefault();
 
-		/**
-		 * Check if there is topic prop (Edit more)
-		 */
-		
-		if(this.props.topic){
+			/**
+			 * Check if there is topic prop (Edit more)
+			 */
+			
+			if(this.props.topic){
 
-			this.getFlux().actions.AdminActions.editMainTopic({
-				id: this.props.topic.id,
-				name: this.state.name,
-				userId: CURRENT_USER.id
-			})
+				this.props.flux.actions.AdminActions.editMainTopic({
+					id: this.props.topic.id,
+					name: this.state.name,
+					userId: CURRENT_USER.id
+				})
 
-		}else{
-		
-			this.getFlux().actions.AdminActions.createMainTopic(this.state)
+			}else{
+			
+				this.props.flux.actions.AdminActions.createMainTopic(this.state)
+			}
+
+			this.props.closeModal.call(this)
+
 		}
-
-		this.props.closeModal.call(this)
 
 	},
 	getDefaultProps: function(){
@@ -64,12 +72,13 @@ var NewTopic = React.createClass({
 				<div className="modal-dialog-title">
 					{this.props.title}
 				</div>
-				<form className="modal-dialog-body" onSubmit = {this.onSave}>
+				<form className="modal-dialog-body" onSubmit = {this.onSave} ref="form">
 					{topic? <input type="hidden" name = 'topicId' value = {topic_id} /> : null}
 					<InputMaterial  
 						ref = "firstInput"
 						label = 'Name of the topic' 
 						defaultValue = {topic_name}
+						required = {true}
 						onChange = { (event) => {
 							this.setState({
 								name: event.target.value
